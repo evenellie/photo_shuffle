8f627e9945404685661b3708e665192518c7f633
Ellen Musick <emkunzeman@gmail.com>
2013-08-20 16:05:47 -0700

    Generate Rails 3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoShuffle::Application.config.secret_token = 'ecc1192f3bb4de9702bebb04e84ff373e42655a1977f21f1343da0e1cb23937fec2c1d0c01e7dba9b037b5bd8898c988e3355614dea2a942bb42526f9982180a'
